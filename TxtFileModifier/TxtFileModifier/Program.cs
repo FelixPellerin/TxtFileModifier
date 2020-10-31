53d3990b896fbaaf110a396b71cd7a7a09f09c21
FelixPellerin <pellerinfelix@yahoo.ca>
2020-10-30 20:24:25 -0400

    Beginning
    started with basic arguments, not implemented yet

---
+++ TxtFileModifier/TxtFileModifier/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace TxtFileModifier
+{
+    class Program
+    {
+        // -F file location
+        // -O output file
+        // -L cut when lenght bigger then x
+        static string FILELOCATION;
+        static string FILEOUTPUT;
+        static void Main(string[] args)
+        {
+            if (args.Length == 0){
+                Console.WriteLine("Arguments Required");
+            }
+            else{
+                int argCursor = 0;
+                while (argCursor < args.Length){
+                    switch (args[argCursor]){
+                        case "-F":
+                            FILELOCATION = args[argCursor + 1];
+                            Console.WriteLine(" File to be read : " + FILELOCATION);
+                            break;
+
+                        case "-O":
+                            FILEOUTPUT = args[argCursor + 1];
+                            Console.WriteLine(" Data will be written to : " + FILEOUTPUT);
+                            break;
+
+                        case "-L":
+                            break;
+                        
+                        default:
+                            Console.WriteLine(" Argument unknown");
+                            break;
+                    };
+                }
+
+            }
+            
+        }
+
+
+
+    }
+}
